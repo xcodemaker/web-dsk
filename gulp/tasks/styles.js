75fce5000feedfdcf1f26ff6cf9281838e3f2afe
xcodemaker <dhammika.dsk@gmail.com>
2018-03-21 16:01:53 +0530

    browsersync added

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,16 @@
+var gulp=require('gulp'),
+postcss=require('gulp-postcss'),
+autoprefixer=require('autoprefixer'),
+cssvars=require('postcss-simple-vars'),
+nested=require('postcss-nested'),
+cssImport=require('postcss-import');
+
+gulp.task("styles",function(){
+    gulp.src("./app/assets/styles/styles.css")
+    .pipe(postcss([cssImport,cssvars,autoprefixer,nested]))
+    .on('error',function(errorInfo){
+        console.log(errorInfo.toString());
+        this.emit('end');
+    })
+    .pipe(gulp.dest('./app/temp/styles'));
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,10 @@
-var gulp=require('gulp');
-var watch=require('gulp-watch'),
-postcss=require('gulp-postcss'),
-autoprefixer=require('autoprefixer'),
-cssvars=require('postcss-simple-vars'),
-nested=require('postcss-nested'),
-cssImport=require('postcss-import');
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
-gulp.task("default",function(){
-    console.log("Hello gulp");
-});
 
-gulp.task("html",function(){
-    console.log("html task perform");
-});
 
-gulp.task("styles",function(){
-    gulp.src("./app/assets/styles/styles.css")
-    .pipe(postcss([cssImport,cssvars,autoprefixer,nested]))
-    .pipe(gulp.dest('./app/temp/styles'));
-});
 
-gulp.task("watch",function(){
-    watch("./app/index.html",function(){
-        gulp.start('html');
-    });
-    watch("./app/assets/styles/**/*.css",function(){
-        gulp.start('styles');
-    });
-});
+
+
 
 
